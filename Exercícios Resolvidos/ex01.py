3fa128217867394a4be189d684c3e455036b0e51
Bernardo Barreto <bernardobarreto2074@gmail.com>
2026-06-19 14:49:15 -0300

    Create ex01.py

---
+++ Exercícios Resolvidos/ex01.py
@@ -0,0 +1,12 @@
+# 1. Defina uma função que leia do teclado uma sequência de números inteiros dados em uma única linha. A função deve retornar uma lista contendo os números que estão na linha.
+
+def ler_sequencia_inteiros():
+    #solicito os números ao usuário
+    entrada = input('Escreva aqui números INTEIROS separados por espaço: ')
+    #transformo essesnuúmeros solicitados inteiros e em uma lista
+    lista_inteiros = [int(numero) for numero in entrada.split()]
+    #retorno o print dessa lista 
+    return print(lista_inteiros)
+    
+#chamo a função
+ler_sequencia_inteiros()
