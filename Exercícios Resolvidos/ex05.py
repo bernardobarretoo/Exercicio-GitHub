b31b67ace8a3bae199a12753a94f1408054b9b35
Bernardo Barreto <bernardobarreto2074@gmail.com>
2026-06-21 20:35:27 -0300

    resolvi o 5o!

--- Exercícios Resolvidos/ex05.py
+++ Exercícios Resolvidos/ex05.py
@@ -1,1 +1,7 @@
 # 5. Dada uma lista com nomes, filtrar palavras maiores que 5 letras
+
+nomes = ['Bernardo','Clara', 'Raquel', 'Valedemar', 'Alberto', 'Guilherme', 'Fernando','Neymar', 'Anita', 'João']
+
+maiores_que_5 = [ nome for nome in nomes if len(nome) > 5]
+
+print(maiores_que_5)
