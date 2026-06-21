96287dc5175b5354585974faa5658951ac616cf3
Bernardo Barreto <bernardobarreto2074@gmail.com>
2026-06-21 16:22:24 -0300

    realização do exercicio 2

--- Exercícios Resolvidos/ex02.py
+++ Exercícios Resolvidos/ex02.py
@@ -1,3 +1,25 @@
 # 2. Defina a função descendentes que pega uma árvore genealógica e retorna todos os descendentes da raiz. Utilize compreensões.
 
+def descendentes(arv):
+    if arv[1] == []:
+        return []
+    else :
+        des = []
+        for filho in arv[1]:
+            des.append(filho[0])
+            des.extend(descendentes(filho))
+        return des
 
+
+
+
+
+árvore = ( 'Rose', [ 
+    ('Márcia', []), 
+    ('Marília', [
+        ('Bernardo', [('Jorge', [])]), 
+        ('Nicole', [])
+        ])
+                                      ])
+
+print(descendentes(árvore))
