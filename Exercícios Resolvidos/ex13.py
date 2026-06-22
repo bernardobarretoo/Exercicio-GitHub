522d4b7021ab5d9a84105948349d105a620bd1ae
Bernardo Barreto <bernardobarreto2074@gmail.com>
2026-06-22 17:10:55 -0300

    exercise completed!

--- Exercícios Resolvidos/ex13.py
+++ Exercícios Resolvidos/ex13.py
@@ -1,1 +1,10 @@
 # 13. Dada uma matriz de inteiros, me faça uma lista com apenas os numeros pares da matriz
+
+
+matriz = [ [1, 2, 3], 
+          [4, 5, 6], 
+          [7, 8, 9] ]
+
+pares = [numero for linha in matriz for numero in linha if numero % 2 == 0]
+
+print(pares)
