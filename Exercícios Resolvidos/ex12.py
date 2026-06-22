14faead66ef286a4337bb03113a43ac940ab6ee0
Bernardo Barreto <bernardobarreto2074@gmail.com>
2026-06-22 17:00:51 -0300

    12a questão feita

--- Exercícios Resolvidos/ex12.py
+++ Exercícios Resolvidos/ex12.py
@@ -1,1 +1,7 @@
 # 12. Crie uma lista que que extraia todas as letras das palavras e gere uma única lista. Ex: palavras = ["python", "java"]. Saída = ['p', 'y', 't', 'h', 'o', 'n', 'j', 'a', 'v', 'a']
+
+palavras = ['motor', 'avião', 'luz']
+
+letras = [letra for palavra in palavras for letra in palavra]
+
+print(letras)
