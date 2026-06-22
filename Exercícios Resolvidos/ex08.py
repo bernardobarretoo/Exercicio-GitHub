15e0ec6552005dc4496a07e02151af1319427a80
Bernardo Barreto <bernardobarreto2074@gmail.com>
2026-06-21 21:31:38 -0300

    Update ex08.py

--- Exercícios Resolvidos/ex08.py
+++ Exercícios Resolvidos/ex08.py
@@ -1,1 +1,5 @@
 # 8. Faça uma lista com os números pares de uma lista de inteiros
+
+inteiros_aleatorios = [2, 23 ,14 ,454 ,45, 48, 878, 9, 43 , 2 , 43, 444, 19, 47, 56, 45, 34, 56, 53, 0, 1, 26]
+pares = [numero for numero in inteiros_aleatorios if numero % 2 == 0]
+print(pares)
