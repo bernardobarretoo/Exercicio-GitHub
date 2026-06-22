760fb1e3743795e8752b8f126dca6fbeb5bc94e5
Bernardo Barreto <bernardobarreto2074@gmail.com>
2026-06-21 21:26:21 -0300

    exercicio 7 feito!

--- Exercícios Resolvidos/ex07.py
+++ Exercícios Resolvidos/ex07.py
@@ -1,1 +1,5 @@
 # 7. Gere uma lista contendo o tamanho de cada palavra. Ex de entrada: ["python", "java", "javascript", "c"]
+
+palavras = list(input('Escreva as palavras separadas por espaço: ').split())
+tamanho = [len(palavra) for palavra in palavras]
+print(tamanho)
